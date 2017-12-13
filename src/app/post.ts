818624f7f73328435e63a19e554360a4942d9387
Timothy R Rager <important.rager.activities@gmail.com>
2017-12-13 13:04:46 -0500

    Stubbed json for Post

--- src/app/post.ts
+++ src/app/post.ts
@@ -4,20 +4,17 @@ export class Post {
   postId: number; // Static generation +1?
   userId: number; // Provided by user service
   threadId: number; // Provided by thread service
-  timestamp: Date; // Generated at construction
+  timestamp: string;//Date; // Generated at construction
   content: string; //
 
   constructor(userId: number, threadId: number, content: string) {
-    this.timestamp = new Date();
+    //this.timestamp = new Date();
+    this.timestamp = 'DateTimeStub: ' + userId + ' ' + threadId + ' ' + 'am/pm';
     this.postId = Post.postIdSeed;
     Post.postIdSeed++;
     this.userId = userId;
     this.threadId = threadId;
     this.content = content;
-  }
-
-  logDate(): void {
-    alert((new Date()).toJSON());
   }
 
 }
