e5acbbb8d87aca08a08f7e24f5526a30ca79b029
Timothy R Rager <important.rager.activities@gmail.com>
2017-12-13 10:33:01 -0500

    Stubbing models and chat components

---
+++ src/app/display-posts-component/display-posts-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-posts-component',
+  templateUrl: './display-posts-component.component.html',
+  styleUrls: ['./display-posts-component.component.css']
+})
+export class DisplayPostsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
